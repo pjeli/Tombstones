983e1edd4e2a883f1d4edf900bf4f7b24c6ae8a8
Plamen Jeliazkov <plamenj2003@gmail.com>
2023-07-03 23:27:02 -0700

    Add slash commands. Add tombstones for levels. Add last_words.

--- Tombstones/Tombstones.lua
+++ Tombstones/Tombstones.lua
@@ -16,9 +16,9 @@ local COMM_COMMANDS = {
 }
 
 -- Variables
-local deathMarkers = {}
 local deathRecordsDB
 local deathRecordCount = 0
+local showMarkers = true
 
 -- Libraries
 local hbdp = LibStub("HereBeDragons-Pins-2.0")
@@ -34,16 +34,14 @@ addon:RegisterEvent("CHAT_MSG_CHANNEL")
 addon:RegisterEvent("CHAT_MSG_ADDON") -- Changed from CHAT_MSG_SAY
 
 -- Add death marker function
-local function AddDeathMarker(mapID, contID, posX, posY, timestamp, user)
+local function AddDeathMarker(mapID, contID, posX, posY, timestamp, user, level)
     if deathRecordCount >= DEATH_RECORD_WINDOW_SIZE then
         table.remove(deathRecordsDB.deathRecords, 1)
-        table.remove(deathMarkers, 1)
         deathRecordCount = deathRecordCount - 1
     end
 
-    local marker = { mapID = mapID, contID = contID, posX = posX, posY = posY, timestamp = timestamp, user = user }
+    local marker = { mapID = mapID, contID = contID, posX = posX, posY = posY, timestamp = timestamp, user = user , level = level, last_words = last_words }
     table.insert(deathRecordsDB.deathRecords, marker)
-    table.insert(deathMarkers, marker)
     deathRecordCount = deathRecordCount + 1
 
     -- Place your custom code here to handle additional logic for the death marker
@@ -52,10 +50,10 @@ local function AddDeathMarker(mapID, contID, posX, posY, timestamp, user)
 end
 
 local function ClearDeathMarkers()
-    for _, marker in ipairs(deathMarkers) do
-        if marker.texture then
-            marker.texture:Hide()
-            marker.texture = nil
+    for _, marker in ipairs(deathRecordsDB.deathRecords) do
+        if (marker.texture ~= nil) then
+            hbdp:RemoveAllWorldMapIcons("Tombstones")
+            --marker.texture:Hide()
         end
     end
 end
@@ -63,7 +61,7 @@ end
 
 local function UpdateWorldMapMarkers()
     local worldMapFrame = WorldMapFrame
-    if worldMapFrame and worldMapFrame:IsVisible() then
+    if showMarkers and worldMapFrame and worldMapFrame:IsVisible() then
         local mapCanvas = worldMapFrame.ScrollContainer.Child
         local mapCanvasScale = mapCanvas:GetEffectiveScale()
         local mapCanvasWidth, mapCanvasHeight = mapCanvas:GetSize()
@@ -76,28 +74,58 @@ local function UpdateWorldMapMarkers()
         C_Timer.After(0.1, function()
             -- Display death markers on the world map
             local currentContinentID = C_Map.GetMapInfo(worldMapFrame:GetMapID()).parentMapID
-            for _, marker in ipairs(deathMarkers) do
-                local mapID, contID, posX, posY = marker.mapID, marker.contID, marker.posX, marker.posY
+            for _, marker in ipairs(deathRecordsDB.deathRecords) do
+                local mapID, contID, posX, posY, level, timestamp = marker.mapID, marker.contID, marker.posX, marker.posY, marker.level, marker.timestamp
 
                 -- Check if the marker is on a different continent
                     -- Create the marker on the current continent's map
                     local markerMapButton = CreateFrame("Button", nil, WorldMapButton)
-                    markerMapButton:SetSize(20, 20) -- Adjust the size of the marker as needed
+                    markerMapButton:SetSize(12, 12) -- Adjust the size of the marker as needed
                     markerMapButton:SetFrameStrata("FULLSCREEN") -- Set the frame strata to ensure it appears above other elements
 
                     markerMapButton.texture = markerMapButton:CreateTexture(nil, "BACKGROUND")
                     markerMapButton.texture:SetAllPoints(true)
-                    markerMapButton.texture:SetTexture("Interface\\Icons\\Ability_Creature_Cursed_03")
-
+                    if (level == nil) then
+                      markerMapButton.texture:SetTexture("Interface\\Icons\\Ability_fiegndead")
+                    elseif (level <= 30) then
+                      markerMapButton.texture:SetTexture("Interface\\Icons\\Ability_Creature_Cursed_03")
+                    elseif (level <= 59) then
+                      markerMapButton.texture:SetTexture("Interface\\Icons\\Spell_holy_nullifydisease")
+                    else
+                      markerMapButton.texture:SetTexture("Interface\\Icons\\Ability_creature_cursed_05")
+                    end
                     -- Set the tooltip text to the name of the player who died
                     markerMapButton:SetScript("OnEnter", function(self)
                        GameTooltip:SetOwner(self, "ANCHOR_BOTTOM")
-                       GameTooltip:SetText("R.I.P. " .. marker.user)
+                       if (marker.level == nil) then
+                           GameTooltip:SetText("R.I.P. " .. marker.user .. " - ?")
+                       else
+                           GameTooltip:SetText("R.I.P. " .. marker.user .. " - " .. marker.level)
+                       end
+                       if (marker.last_words ~= nil) then
+                           GameTooltip:AddLine(marker.last_words, 1, 0, 0)
+                       end
                        GameTooltip:Show()
                     end)
                     markerMapButton:SetScript("OnLeave", function(self)
                        GameTooltip:Hide()
                     end)
+
+                    if (marker.last_words ~= nil) then
+                       local borderTexture = markerMapButton:CreateTexture(nil, "OVERLAY")
+                       borderTexture:SetAllPoints(markerMapButton)
+                       borderTexture:SetTexture("Interface\\Cooldown\\ping4")
+                       borderTexture:SetBlendMode("ADD")
+                       borderTexture:SetVertexColor(1, 1, 0, 0.7) -- Set the border color to yellow (RGB values)
+                    end
+
+                    -- Check if the marker occurred within the last 24 hours
+                    local currentTime = time()
+                    local timeDifference = currentTime - timestamp
+                    local secondsIn24Hours = 12 * 60 * 60 -- 24 hours in seconds
+                    if timeDifference >= secondsIn24Hours then
+                       markerMapButton.texture:SetVertexColor(.5, .5, .5, 0.5)
+                    end
 
                     -- Add the marker to the current continent's map
                     hbdp:AddWorldMapIconMap("Tombstones", markerMapButton, mapID, posX, posY, HBD_PINS_WORLDMAP_SHOW_WORLD)
@@ -128,11 +156,17 @@ local function LoadDeathRecords()
         deathRecordsDB.version = ADDON_SAVED_VARIABLES_VERSION
         deathRecordsDB.deathRecords = {}
         deathRecordsCount = 0
-        deathMarkers = {}
     else
         deathRecordsCount = #deathRecordsDB.deathRecords
-        deathMarkers = deathRecordsDB.deathRecords
     end
+end
+
+local function ClearDeathRecords()
+    deathRecordsDB = {}
+    deathRecordsDB.version = ADDON_SAVED_VARIABLES_VERSION
+    deathRecordsDB.deathRecords = {}
+    deathRecordsCount = 0
+    _G["deathRecordsDB"] = deathRecordsDB
 end
 
 function addon:SendMessage(message, distribution, target)
@@ -166,13 +200,28 @@ function addon:OnCommReceived(prefix, message, distribution, sender)
     end
 end
 
+function TdeathlogReceiveLastWords(sender, data)
+  if data == nil then return end
+  local values = {}
+  for w in data:gmatch("(.-)~") do table.insert(values, w) end
+  local msg = values[2]
+
+  -- Iterate over the death records
+  for index, record in ipairs(deathRecordsDB.deathRecords) do
+    if record.user == sender then
+        record.last_words = msg
+        break
+    end
+  end
+end
+
 function TdeathlogReceiveChannelMessage(sender, data)
   if data == nil then return end
   local decoded_player_data = TdecodeMessage(data)
   print("Tombstones decoded a DeathLog death for " .. sender .. "!")
   if sender ~= decoded_player_data["name"] then return end
   local x, y = strsplit(",", decoded_player_data["map_pos"],2)
-  AddDeathMarker(tonumber(decoded_player_data["map_id"]), tonumber(decoded_player_data["cont_id"]), tonumber(x), tonumber(y), tonumber(decoded_player_data["date"]), sender)
+  AddDeathMarker(tonumber(decoded_player_data["map_id"]), tonumber(decoded_player_data["cont_id"]), tonumber(x), tonumber(y), tonumber(decoded_player_data["date"]), sender, tonumber(decoded_player_data["level"]))
 end
 
 function TdecodeMessage(msg)
@@ -217,6 +266,31 @@ function TPlayerData(name, guild, source_id, race_id, class_id, level, instance_
     ["last_words"] = last_words,
   }
 end
+
+-- Define slash commands
+SLASH_TOMBSTONES1 = "/tombstones"
+SLASH_TOMBSTONES2 = "/ts"
+
+-- Slash command handler function
+local function SlashCommandHandler(msg)
+    -- Process the command and perform the desired actions
+    if msg == "show" then
+        -- Show death markers
+        showMarkers = true
+    elseif msg == "hide" then
+        showMarkers = false
+        ClearDeathMarkers()
+    elseif msg == "clear" then
+        -- Clear all death records
+        ClearDeathRecords()
+    else
+        -- Display command usage information
+        print("Usage: /tombstones or /ts [show | hide | clear]")
+    end
+end
+
+-- Register slash command handler
+SlashCmdList["TOMBSTONES"] = SlashCommandHandler
 
 -- Initialize
 addon:SetScript("OnEvent", function(self, event, ...)
@@ -276,6 +350,7 @@ addon:SetScript("OnEvent", function(self, event, ...)
     if command == COMM_COMMANDS["LAST_WORDS"] then
       local player_name_short, _ = string.split("-", arg[2])
       print("Receiving DeathLog last_words ping for " .. player_name_short .. ".")
+      TdeathlogReceiveLastWords(player_name_short, msg)
     end
   end
 end)
